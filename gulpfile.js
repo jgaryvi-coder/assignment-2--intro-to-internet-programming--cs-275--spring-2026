aab5b59eaf4d7080a9ac32d2f245b3916b9a60f9
jgaryvi-coder <jgaryvi@gmail.com>
2026-03-10 12:59:24 -0400

    Prepared and installed Gulp module

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-const { src, dest } = require(`gulp`);
-const htmlCompressor = require(`gulp-htmlmin`);
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`),
+    { deleteAsync } = require(`del`),
+    babel = require(`gulp-babel`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    htmlValidator = require(`gulp-html`),
+    jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
+    sass = require(`gulp-sass`)(require(`sass`)),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
 
+let browserChoice = `default`;
 let compressHTML = () => {
     return src(`*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
